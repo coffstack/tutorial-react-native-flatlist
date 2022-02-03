14e7c077498d2dc5bd6782fd90b64214c44665fa
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-02-03 15:27:58 -0300

    SeparatorItem

---
+++ components/SeparatorItem/SeparatorItem.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { View } from "react-native";
+
+export function SeparatorItem() {
+  return (
+    <View style={{ height: 1, width: "100%", backgroundColor: "#b4835f97" }} />
+  );
+}
